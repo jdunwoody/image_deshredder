c0e17a03664fd7dcc389e01457e2c002c71bc74b
James Dunwoody <jamesd@hooroo.com>
2013-07-06 00:02:51 +1000

    Nicer separation between source sections and destionation pixels

--- lib/shredder.rb
+++ lib/shredder.rb
@@ -1,27 +1,45 @@
 class Shredder
+  attr_accessor :number_of_sections
+
   def from_file(in_file, out_file)
-    image = Magick::Image::read(in_file).first
+    source = SourceImage.new(in_file)
+    destination = DestinationImage.new(source.columns, source.rows)
 
-    offset_start = Random.rand(image.columns)
-    offset_width = Random.rand(offset_start..image.columns) - offset_start
+    destination_x = 0
 
-    (0..image.columns).each do |x|
-      (0..image.rows).each do |y|
-        if x >= offset_start && x <= offset_start + offset_width
-          from = image.pixel_color(x, y)
-          to = image.pixel_color(x + offset_width, y)
+    source.sections.each do |section|
+      source_x = section.x
 
-          image.pixel_color(x + offset_width, y, from)
-          image.pixel_color(x, y, to)
-        end
+      (0..source.rows).each do |y|
+        destination.pixel_color(destination_x,
+                                y,
+                                source.pixel_color(source_x,y))
       end
+      destination_x = destination_x + 1
     end
 
-    image.write(out_file)
+    destination.write(out_file)
   end
 end
 
+class SourceImage
+  def initialize(file)
+    @image_magick_image= Magick::Image::read(file).first
+  end
 
+  def sections
+    [50,100,150,200,300]
+  end
+end
+
+class DestinationImage
+  def initialize(columns, rows)
+    @image_magick_image = Magick::Image.new(columns, rows)
+  end
+end
+
+#offset_start = Random.rand(source.columns)
+#offset_width = 100 #Random.rand(offset_start..source.columns) - offset_start
 #(0..img.columns).each
 #pixels = img.get_pixels(0,0,img.width, img.height)
 #binding.pry
