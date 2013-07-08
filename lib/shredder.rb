b13dd63c98fb2a7adf8c854b6d818849e57c5d66
James Dunwoody <jamesd@hooroo.com>
2013-07-08 22:32:49 +1000

    Schreding works with variable widths and sizes

--- lib/shredder.rb
+++ lib/shredder.rb
@@ -1,6 +1,6 @@
-class Shredder
-  attr_accessor :number_of_sections
+require 'ostruct'
 
+class Shredder
   def from_file(in_file, out_file)
     source = SourceImage.new(in_file)
     destination = DestinationImage.new(source.columns, source.rows)
@@ -8,14 +8,14 @@ class Shredder
     destination_x = 0
 
     source.sections.each do |section|
-      source_x = section.x
-
-      (0..source.rows).each do |y|
-        destination.pixel_color(destination_x,
-                                y,
-                                source.pixel_color(source_x,y))
+      (section.from..section.to).each do |x|
+        (0..source.rows).each do |y|
+          destination.pixel_color(destination_x,
+                                  y,
+                                  source.pixel_color(x,y))
+        end
+        destination_x = destination_x + 1
       end
-      destination_x = destination_x + 1
     end
 
     destination.write(out_file)
@@ -27,14 +27,56 @@ class SourceImage
     @image_magick_image= Magick::Image::read(file).first
   end
 
-  def sections
-    [50,100,150,200,300]
+  def rows
+    @image_magick_image.rows
   end
+
+  def columns
+    @image_magick_image.columns
+  end
+
+  def sections
+    x_start = 0
+    puts "#{@image_magick_image.columns}x#{@image_magick_image.rows}"
+
+    sections = []
+    while x_start < @image_magick_image.columns do
+      x_end = [x_start + rand(@image_magick_image.columns / 10),
+               @image_magick_image.columns].min
+
+      section = Section.new(from: x_start,
+                            to: x_end)
+
+      puts "section #{section.from},#{section.to}"
+      x_start = x_end
+      sections << section
+    end
+
+    sections.shuffle
+  end
+
+  #number_of_sections = rand(10..30)
+  #sections = (1..number_of_sections).map do |section_number|
+
+  def pixel_color(x,y)
+    @image_magick_image.pixel_color(x,y)
+  end
+end
+
+class Section < OpenStruct
 end
 
 class DestinationImage
   def initialize(columns, rows)
     @image_magick_image = Magick::Image.new(columns, rows)
+  end
+
+  def pixel_color(x,y,pixel)
+    @image_magick_image.pixel_color(x,y,pixel)
+  end
+
+  def write(file)
+    @image_magick_image.write(file)
   end
 end
 
